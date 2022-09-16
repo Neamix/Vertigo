db2092ba44522887b4b3728ac5aaf77167c1b9c9
NEAMIX\abdal <abdalrhmanhussin44@gmail.com>
2022-09-16 10:00:29 +0200

    Attending delete Validator

--- app/GraphQL/Validators/Mutation/AttendingdeleteValidator.php
+++ app/GraphQL/Validators/Mutation/AttendingdeleteValidator.php
@@ -2,6 +2,8 @@
 
 namespace App\GraphQL\Validators\Mutation;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Nuwave\Lighthouse\Validation\Validator;
 
 final class AttendingdeleteValidator extends Validator
@@ -14,7 +16,14 @@ final class AttendingdeleteValidator extends Validator
     public function rules(): array
     {
         return [
-            // TODO Add your validation rules
+            'id' => ['exists:requests,id'],
+            'password' => ['required',
+                function($attribute,$value,$fail) {
+                    if ( ! Hash::check($value,Auth::user()->password) ) {
+                        return $fail('Wrong password,please check your password and try again');
+                    }
+                }
+            ],
         ];
     }
 }
